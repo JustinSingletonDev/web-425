e1c130527831088dfa301ce10a210372ecbed5c0
Justin Singleton <jsingleton@my365.bellevue.edu>
2020-02-01 14:07:55 -0600

    Add assignment5.2

---
+++ week-5/singleton-md-toolbar/src/app/app.module.ts
@@ -0,0 +1,36 @@
+/*
+============================================
+; Title: Assignment 5.2
+; Author: Justin Singleton
+; Date: 31 January 2020
+; Description: This program demonstrates the
+; use of Angular Material to create a
+; navigation toolbar
+;===========================================
+*/
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
